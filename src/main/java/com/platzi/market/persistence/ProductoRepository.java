2ed9149dcfbaaefb8bed341a5101efc3c16f99cf
Ricardo Patino <rpatino12@outlook.com>
2023-04-13 16:47:46 -0500

    Query methods

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -4,6 +4,7 @@ import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     // We inject the repository interface that extends from CrudRepository to get all the methods from there
@@ -12,5 +13,13 @@ public class ProductoRepository {
     // Let's create a method to get all the products from the database
     public List<Producto> getAll() {
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategory(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getScarce(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
     }
 }
--- src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -1,7 +1,20 @@
 package com.platzi.market.persistence.crud;
 
 import com.platzi.market.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Long> {
+    // We can use Natives queries with SQL using the @Query annotation
+    // @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+
+    // But is better to use the query methods, you just have to follow the notation correctly when naming the method
+    // Let's create query methods that let us make queries only with the name of the method
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    // This query method find the products that are active (estado = true) and are the last units in the inventory
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
