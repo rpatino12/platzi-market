a7835af5e6b7f4cab5a5b0191be7d1e70f7e52ad
Ricardo Patino <rpatino12@outlook.com>
2023-04-14 18:48:16 -0500

    Rest Controller class ProductController finished

--- src/main/java/com/platzi/market/web/controller/ProductController.java
+++ src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -3,6 +3,11 @@ package com.platzi.market.web.controller;
 import com.platzi.market.domain.Product;
 import com.platzi.market.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,23 +20,28 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @GetMapping
     public List<Product> getALl() {
         return productService.getALl();
     }
 
-    public Optional<Product> getProduct(long productId) {
+    @GetMapping("/{id}")
+    public Optional<Product> getProduct(@PathVariable("id") long productId) {
         return productService.getProduct(productId);
     }
 
-    public Optional<List<Product>> getByCategory(int categoryId) {
+    @GetMapping("/by-category/{categoryId}")
+    public Optional<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId) {
         return productService.getByCategory(categoryId);
     }
 
-    public Product save(Product product) {
+    @PostMapping("/save")
+    public Product save(@RequestBody Product product) {
         return productService.save(product);
     }
 
-    public boolean delete(long productId) {
+    @DeleteMapping("/delete/{id}")
+    public boolean delete(@PathVariable("id") long productId) {
         return productService.delete(productId);
     }
 }
