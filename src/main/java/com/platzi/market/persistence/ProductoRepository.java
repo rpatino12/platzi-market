7944bd5c0530aed57ee3961e416c2a9a708a662a
Ricardo Patino <rpatino12@outlook.com>
2023-04-14 16:17:27 -0500

    Change of the ProductoRepository class to be domain oriented, implementing the ProductRepository interface

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -1,44 +1,59 @@
 package com.platzi.market.persistence;
 
+import com.platzi.market.domain.Product;
+import com.platzi.market.domain.repository.ProductRepository;
 import com.platzi.market.persistence.crud.ProductoCrudRepository;
 import com.platzi.market.persistence.entity.Producto;
+import com.platzi.market.persistence.mapper.ProductMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class ProductoRepository {
+public class ProductoRepository implements ProductRepository {
     // We inject the repository interface that extends from CrudRepository to get all the methods from there
     private ProductoCrudRepository productoCrudRepository;
+    // Let's inject the mapper object
+    private ProductMapper mapper;
 
     // Let's create a method to get all the products from the database
-    public List<Producto> getAll() {
-        return (List<Producto>) productoCrudRepository.findAll();
+    @Override
+    public List<Product> getALl() {
+        List<Producto> productos = (List<Producto>) productoCrudRepository.findAll();
+        return mapper.toProducts(productos);
     }
 
     // This method gets the products that are part of a category
-    public List<Producto> getByCategory(int idCategoria){
-        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        List<Producto> productos = productoCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId);
+        return Optional.of(mapper.toProducts(productos));
     }
 
     // This method get the products that are close to be unavailable (that are going to end)
-    public Optional<List<Producto>> getScarce(int cantidad){
-        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    @Override
+    public Optional<List<Product>> getScarce(int quantity) {
+        Optional<List<Producto>> productos = productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity, true);
+        return productos.map(prods -> mapper.toProducts(prods));
     }
 
     // This method get a product by its id
-    public Optional<Producto> getProduct(Long idProducto){
-        return productoCrudRepository.findById(idProducto);
+    @Override
+    public Optional<Product> getProduct(long productId) {
+        return productoCrudRepository.findById(productId).map(producto -> mapper.toProduct(producto));
     }
 
     // This method save a product in the database
-    public Producto save(Producto producto){
-        return productoCrudRepository.save(producto);
+    @Override
+    public Product save(Product product) {
+        Producto producto = mapper.toProducto(product);
+        return mapper.toProduct(productoCrudRepository.save(producto));
     }
 
     // This method deletes a product by its id
-    public void delete(Long idProducto){
-        productoCrudRepository.deleteById(idProducto);
+    @Override
+    public void delete(long productId) {
+        productoCrudRepository.deleteById(productId);
     }
 }
