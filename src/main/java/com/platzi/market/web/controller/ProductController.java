408ce070091742e25069ada51c3657ea74b48bfc
Ricardo Patino <rpatino12@outlook.com>
2023-04-14 18:13:49 -0500

    Create the class ProductController to set the API Rest Controller

---
+++ src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -0,0 +1,38 @@
+package com.platzi.market.web.controller;
+
+import com.platzi.market.domain.Product;
+import com.platzi.market.domain.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    public List<Product> getALl() {
+        return productService.getALl();
+    }
+
+    public Optional<Product> getProduct(long productId) {
+        return productService.getProduct(productId);
+    }
+
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        return productService.getByCategory(categoryId);
+    }
+
+    public Product save(Product product) {
+        return productService.save(product);
+    }
+
+    public boolean delete(long productId) {
+        return productService.delete(productId);
+    }
+}
