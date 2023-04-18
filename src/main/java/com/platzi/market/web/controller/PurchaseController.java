3685d310fb54ad108c8d02b29a69729b9c32bd36
Ricardo Patino <rpatino12@outlook.com>
2023-04-18 18:44:31 -0500

    Controller and Service class created for Purchase table

---
+++ src/main/java/com/platzi/market/web/controller/PurchaseController.java
@@ -0,0 +1,39 @@
+package com.platzi.market.web.controller;
+
+import com.platzi.market.domain.Purchase;
+import com.platzi.market.domain.service.PurchaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/purchases")
+public class PurchaseController {
+    @Autowired
+    private PurchaseService purchaseService;
+
+    @GetMapping
+    public ResponseEntity<List<Purchase>> get(){
+        return new ResponseEntity<>(purchaseService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/client/{id}")
+    public ResponseEntity<List<Purchase>> getById(@PathVariable("id") String clientId){
+        return purchaseService.getByClient(clientId)
+                .map(purchases -> new ResponseEntity<>(purchases, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Purchase> save(@RequestBody Purchase purchase){
+        return new ResponseEntity<>(purchaseService.save(purchase), HttpStatus.CREATED);
+    }
+}
