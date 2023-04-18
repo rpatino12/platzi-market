900a12d9150e58e044af5f19104ba4904d9d3af4
Ricardo Patino <rpatino12@outlook.com>
2023-04-18 17:23:11 -0500

    Domain model created for the purchase class

---
+++ src/main/java/com/platzi/market/domain/PurchaseItem.java
@@ -0,0 +1,40 @@
+package com.platzi.market.domain;
+
+public class PurchaseItem {
+    private long productId;
+    private int quantity;
+    private double total;
+    private boolean active;
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
