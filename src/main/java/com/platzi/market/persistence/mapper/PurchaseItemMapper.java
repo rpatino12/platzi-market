f8959093eb91bf89fe7a22b0ebbff7a639e56fdd
Ricardo Patino <rpatino12@outlook.com>
2023-04-18 17:53:12 -0500

    Mappers created for PurchaseItem and Purchase classes

---
+++ src/main/java/com/platzi/market/persistence/mapper/PurchaseItemMapper.java
@@ -0,0 +1,30 @@
+package com.platzi.market.persistence.mapper;
+
+import com.platzi.market.domain.PurchaseItem;
+import com.platzi.market.persistence.entity.ComprasProducto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring", uses = ProductMapper.class)
+public interface PurchaseItemMapper {
+    @Mappings({
+            @Mapping(source = "id.idProducto", target = "productId"),
+            @Mapping(source = "cantidad", target = "quantity"),
+            @Mapping(source = "total", target = "total"),
+            @Mapping(source = "estado", target = "active")
+    })
+    PurchaseItem toPurchaseItem(ComprasProducto comprasProducto);
+
+    // For this conversion we have to ignore the attributes we're not converting from ComprasProducto
+    // We have to be aware that the PK of ComprasProducto is complex
+    // So we have to ignore the other attribute inside that PK (idCompra)
+    @InheritInverseConfiguration
+    @Mappings({
+            @Mapping(target = "id.idCompra", ignore = true),
+            @Mapping(target = "compra", ignore = true),
+            @Mapping(target = "producto", ignore = true) // We have to use (uses = ProductMapper.class)
+    })
+    ComprasProducto toComprasProducto(PurchaseItem purchaseItem);
+}
---
+++ src/main/java/com/platzi/market/persistence/mapper/PurchaseMapper.java
@@ -0,0 +1,32 @@
+package com.platzi.market.persistence.mapper;
+
+import com.platzi.market.domain.Purchase;
+import com.platzi.market.persistence.entity.Compra;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = PurchaseItemMapper.class)
+public interface PurchaseMapper {
+
+    @Mappings({
+            @Mapping(source = "idCompra", target = "purchaseId"),
+            @Mapping(source = "idCliente", target = "clientId"),
+            @Mapping(source = "fecha", target = "date"),
+            @Mapping(source = "medioPago", target = "paymentMethod"),
+            @Mapping(source = "comentario", target = "comment"),
+            @Mapping(source = "estado", target = "state"),
+            @Mapping(source = "productos", target = "items")
+    })
+    Purchase toPurchase(Compra compra);
+    List<Purchase> toPurchases(List<Compra> compras);
+
+    // The target class in the inverse conversion, must have all the mappings set above (in this case line 16 to 22)
+    // If not, you have to ignore the mapping not contained in the class Purchase
+    @InheritInverseConfiguration
+    @Mapping(target = "cliente", ignore = true)
+    Compra toCompra(Purchase purchase);
+}
