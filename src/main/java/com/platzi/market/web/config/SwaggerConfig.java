2c937ff9af019c7fb99f8fac49e1287c19f052e7
Ricardo Patino <rpatino12@outlook.com>
2023-04-20 13:53:04 -0500

    Added documentation for our API with swagger2, but it's not compatible with spring boot 3 and java 17

---
+++ src/main/java/com/platzi/market/web/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.platzi.market.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+public class SwaggerConfig {
+    // Here we create a bean that is going to return a docket with the documentation of our API
+    // We select the endpoints to be presented in the HTML
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.platzi.market.web.controller")) // The selection of our controllers
+                .build();
+    }
+}
--- src/main/java/com/platzi/market/web/controller/HelloWorldController.java
+++ src/main/java/com/platzi/market/web/controller/HelloWorldController.java
@@ -3,9 +3,11 @@ package com.platzi.market.web.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequestMapping("/sayhi")
+@ApiIgnore
 public class HelloWorldController {
 
     @GetMapping("/hello")
